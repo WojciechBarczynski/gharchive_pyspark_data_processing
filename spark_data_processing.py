8e8279fd3d76fa0ae807f8c8f682860912553f3c
Wojciech Barczyński <barczynski.w@gmail.com>
2022-04-25 13:11:34 +0200

    add comments

--- spark_data_processing.py
+++ spark_data_processing.py
@@ -5,14 +5,18 @@ from pyspark.sql import SparkSession
 import pandas as pd
 
 
-def get_stats(repo_name, year_string, month_string):
+def get_stats(full_repo_name, year_string, month_string):
+    # e.g. full_repo_name: 'apache/spark', year_string: '2021', month_string = '05'
+
     failed_processing_pd_df = pd.DataFrame(columns=['date'])
     failed_processing_index = 0
-    spark = SparkSession.builder.appName('github_repo_stats').getOrCreate()
+
     repo_stats_pd_df = pd.DataFrame(columns=['day', 'watch_events_day_count', 'pull_request_events_day_count'])
     day_index = 0
 
     days_in_month = utills.get_number_of_days_in_month(month_string, year_string)
+
+    spark = SparkSession.builder.appName('github_repo_stats').getOrCreate()
 
     for day in range(1, days_in_month + 1):
         day_string = str(day)
@@ -30,7 +34,7 @@ def get_stats(repo_name, year_string, month_string):
 
             try:
                 os.system(f'wget {gh_archive_url}')
-            except:
+            except:  # TO DO: change except for more precise exception handling
                 failed_processing_pd_df.loc[failed_processing_index] = \
                     [f'{year_string}-{month_string}-{day_string}-{hour_string}']
                 failed_processing_index += 1
@@ -40,14 +44,14 @@ def get_stats(repo_name, year_string, month_string):
 
             try:
                 hour_gh_activity_spark_df = spark.read.json(json_url)
-            except:
+            except:  # TO DO: change except for more precise exception handling
                 failed_processing_pd_df.loc[failed_processing_index] = \
                     [f'{year_string}-{month_string}-{day_string}-{hour_string}']
                 failed_processing_index += 1
                 continue
 
             hour_gh_activity_spark_df = hour_gh_activity_spark_df.select(['type', 'repo.name', 'actor.id'])
-            hour_gh_activity_spark_df = hour_gh_activity_spark_df.filter(f'name == "{repo_name}"')
+            hour_gh_activity_spark_df = hour_gh_activity_spark_df.filter(f'name == "{full_repo_name}"')
             pull_request_events_spark_df = hour_gh_activity_spark_df.filter('type == "PullRequestEvent"')
             watch_events_spark_df = hour_gh_activity_spark_df.filter('type == "WatchEvent"')
             watch_events_spark_df = watch_events_spark_df.select('id').distinct()
